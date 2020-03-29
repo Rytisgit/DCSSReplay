d2433c658bc6d8398b4762c90083f2fadfe3325e
Aspectus <eextreem0@gmail.com>
2020-03-29 16:39:05 +0300

    Combined Console and Display window
    Fixed minimize crash bug
    Deleted window project

---
+++ ShinyConsole/Form2.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace ShinyConsole
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox2.BackColor = System.Drawing.Color.Black;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(1602, 759);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(558, 301);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1044, 458);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1602, 759);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Shown += new System.EventHandler(this.Form_Shown);
+            this.ResizeEnd += new System.EventHandler(this.OnResize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        public System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        public System.Windows.Forms.PictureBox pictureBox2;
+        public System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ ShinyConsole/Form2.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ShinyConsole
+{
+    public partial class Form2 : Form
+    {
+        private delegate void SafeCallDelegate(Bitmap frame);
+        private delegate void SafeCallDelegate2(Bitmap frame);
+        public delegate void MyDelegate();
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public void update(Bitmap frame)
+        {
+            if (pictureBox1.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(update);
+                pictureBox1.Invoke(d, new object[] { frame });
+            }
+            else
+            {
+                pictureBox1.Image = frame;
+            }
+
+        }
+        private void OnResize(object sender, System.EventArgs e)
+        {
+            pictureBox1.Width = (int)(0.35 * ClientSize.Width);
+            pictureBox1.Height = (int)(0.3 * ClientSize.Height);
+            pictureBox1.Location = new System.Drawing.Point((int)(ClientSize.Width*0.65),(int)(0.7 * ClientSize.Height));
+        }
+        private void Form_Shown(Object sender, EventArgs e)
+        {
+
+            ClientSize = new Size(1602, 1050);
+            pictureBox1.Width = (int)(0.35 * ClientSize.Width);
+            pictureBox1.Height = (int)(0.3 * ClientSize.Height);
+            pictureBox1.Location = new System.Drawing.Point((int)(ClientSize.Width * 0.65), (int)(0.7 * ClientSize.Height));
+        }
+
+        public void update2(Bitmap frame)
+        {
+            if (pictureBox2.InvokeRequired)
+            {
+                var d = new SafeCallDelegate2(update2);
+                pictureBox2.Invoke(d, new object[] { frame });
+            }
+            else
+            {
+                pictureBox2.Image = frame;
+            }
+
+        }
+    }
+}
