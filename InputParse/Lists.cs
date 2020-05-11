5f234601fc52019a4293be661698214c23e72a41
Rytis Petronis <petronisrytis@gmail.com>
2020-05-11 22:21:23 +0300

    skaisharprefarctor

--- InputParse/Lists.cs
+++ InputParse/Lists.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
+using SkiaSharp;
 
 namespace InputParser
 {
@@ -12,7 +12,7 @@ namespace InputParser
         ConsoleSwitch,
         ConsoleFull
     }
-    public enum ColorList2
+    public enum ColorListEnum
     {
         BLACK,//0 (black if sleeping? color is background for monster list?)
         RED,//1
@@ -41,47 +41,47 @@ namespace InputParser
     }
     public class ColorList
     {
-        public SolidBrush BLACK = new SolidBrush(Color.FromArgb(0, 0, 0));
-        public SolidBrush RED = new SolidBrush(Color.FromArgb(255, 0, 0));
-        public SolidBrush GREEN = new SolidBrush(Color.FromArgb(0, 255, 0));
-        public SolidBrush BROWN = new SolidBrush(Color.FromArgb(165, 91, 0));
-        public SolidBrush BLUE = new SolidBrush(Color.FromArgb(0, 64, 255));
-        public SolidBrush MAGENTA = new SolidBrush(Color.FromArgb(192, 0, 255));
-        public SolidBrush CYAN = new SolidBrush(Color.FromArgb(0, 255, 255));
-        public SolidBrush LIGHTGREY = new SolidBrush(Color.FromArgb(192, 192, 192));
-        public SolidBrush DARKGREY = new SolidBrush(Color.FromArgb(128, 128, 128));
-        public SolidBrush LIGHTRED = new SolidBrush(Color.FromArgb(255, 128, 128));
-        public SolidBrush LIGHTGREEN = new SolidBrush(Color.FromArgb(128, 255, 128));
-        public SolidBrush YELLOW = new SolidBrush(Color.FromArgb(255, 255, 0));
-        public SolidBrush LIGHTBLUE = new SolidBrush(Color.FromArgb(128, 128, 255));
-        public SolidBrush LIGHTMAGENTA = new SolidBrush(Color.FromArgb(255, 128, 255));
-        public SolidBrush LIGHTCYAN = new SolidBrush(Color.FromArgb(64, 255, 255));
-        public SolidBrush WHITE = new SolidBrush(Color.FromArgb(255, 255, 255));
-        public SolidBrush WHITE2 = new SolidBrush(Color.FromArgb(255, 255, 255));
+        public SKColor BLACK = new SKColor(0, 0, 0);
+        public SKColor RED = new SKColor(255, 0, 0);
+        public SKColor GREEN = new SKColor(0, 255, 0);
+        public SKColor BROWN = new SKColor(165, 91, 0);
+        public SKColor BLUE = new SKColor(0, 64, 255);
+        public SKColor MAGENTA = new SKColor(192, 0, 255);
+        public SKColor CYAN = new SKColor(0, 255, 255);
+        public SKColor LIGHTGREY = new SKColor(192, 192, 192);
+        public SKColor DARKGREY = new SKColor(128, 128, 128);
+        public SKColor LIGHTRED = new SKColor(255, 128, 128);
+        public SKColor LIGHTGREEN = new SKColor(128, 255, 128);
+        public SKColor YELLOW = new SKColor(255, 255, 0);
+        public SKColor LIGHTBLUE = new SKColor(128, 128, 255);
+        public SKColor LIGHTMAGENTA = new SKColor(255, 128, 255);
+        public SKColor LIGHTCYAN = new SKColor(64, 255, 255);
+        public SKColor WHITE = new SKColor(255, 255, 255);
+        public SKColor WHITE2 = new SKColor(255, 255, 255);
 
-        public static Color GetColor(string name)
+        public static SKColor GetColor(string name)
         {
             return name switch
             {
-                "BLACK" => Color.FromArgb(0, 0, 0),
-                "RED" => Color.FromArgb(255, 0, 0),
-                "GREEN" => Color.FromArgb(0, 255, 0),
-                "BROWN" => Color.FromArgb(165, 91, 0),
-                "BLUE" => Color.FromArgb(0, 64, 255),
-                "MAGENTA" => Color.FromArgb(192, 0, 255),
-                "CYAN" => Color.FromArgb(0, 255, 255),
-                "LIGHTGREY" => Color.FromArgb(192, 192, 192),
-                "DARKGREY" => Color.FromArgb(128, 128, 128),
-                "LIGHTRED" => Color.FromArgb(255, 128, 128),
-                "LIGHTGREEN" => Color.FromArgb(128, 255, 128),
-                "YELLOW" => Color.FromArgb(255, 255, 0),
-                "LIGHTBLUE" => Color.FromArgb(128, 128, 255),
-                "LIGHTMAGENTA" => Color.FromArgb(255, 128, 255),
-                "LIGHTCYAN" => Color.FromArgb(64, 255, 255),
-                "WHITE" => Color.FromArgb(255, 255, 255),
-                "WHITE2" => Color.FromArgb(255, 255, 255),
-                null => Color.FromArgb(0, 0, 0),
-                "" => Color.FromArgb(0, 0, 0),
+                "BLACK" => new SKColor(0, 0, 0),
+                "RED" => new SKColor(255, 0, 0),
+                "GREEN" => new SKColor(0, 255, 0),
+                "BROWN" => new SKColor(165, 91, 0),
+                "BLUE" => new SKColor(0, 64, 255),
+                "MAGENTA" => new SKColor(192, 0, 255),
+                "CYAN" => new SKColor(0, 255, 255),
+                "LIGHTGREY" => new SKColor(192, 192, 192),
+                "DARKGREY" => new SKColor(128, 128, 128),
+                "LIGHTRED" => new SKColor(255, 128, 128),
+                "LIGHTGREEN" => new SKColor(128, 255, 128),
+                "YELLOW" => new SKColor(255, 255, 0),
+                "LIGHTBLUE" => new SKColor(128, 128, 255),
+                "LIGHTMAGENTA" => new SKColor(255, 128, 255),
+                "LIGHTCYAN" => new SKColor(64, 255, 255),
+                "WHITE" => new SKColor(255, 255, 255),
+                "WHITE2" => new SKColor(255, 255, 255),
+                null => new SKColor(0, 0, 0),
+                "" => new SKColor(0, 0, 0),
                 _ => throw new ArgumentException($"No Color with name: {name} is defined"),
                 
             };
