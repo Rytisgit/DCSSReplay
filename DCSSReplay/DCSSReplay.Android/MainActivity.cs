a4f90949f8cd90d4af391f3796fd8405218a91e4
Rytis Petronis <petronisrytis@gmail.com>
2020-05-11 22:21:38 +0300

    add xamarin project

---
+++ DCSSReplay/DCSSReplay.Android/MainActivity.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using FrameGenerator;
+using System.Threading.Tasks;
+using ICSharpCode.SharpZipLib.Zip;
+using System.IO;
+
+namespace DCSSReplay.Droid
+{
+    [Activity(Label = "DCSSReplay", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+            
+            base.OnCreate(savedInstanceState);
+            UnzipFileAsync(@"", @".").Wait();
+            var extraFile = Assets.Open("Extra.zip");
+            var gen = new MainGenerator();
+            var img = gen.GenerateImage(null);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+        private async Task<bool> UnzipFileAsync(string zipFilePath, string unzipFolderPath)
+        {
+            try
+            {
+                var entry = new ZipEntry(Path.GetFileNameWithoutExtension(zipFilePath));
+                FileStream fileStreamIn = (FileStream)Assets.Open("Extra.zip");
+                var zipInStream = new ZipInputStream(fileStreamIn);
+                entry = zipInStream.GetNextEntry();
+                while (entry != null && entry.CanDecompress)
+                {
+                    var outputFile = unzipFolderPath + @"/" + entry.Name;
+                    var outputDirectory = Path.GetDirectoryName(outputFile);
+                    if (!Directory.Exists(outputDirectory))
+                    {
+                        Directory.CreateDirectory(outputDirectory);
+                    }
+
+                    if (entry.IsFile)
+                    {
+                        var fileStreamOut = new FileStream(outputFile, FileMode.Create, FileAccess.Write);
+                        int size;
+                        byte[] buffer = new byte[4096];
+                        do
+                        {
+                            size = await zipInStream.ReadAsync(buffer, 0, buffer.Length);
+                            await fileStreamOut.WriteAsync(buffer, 0, size);
+                        } while (size > 0);
+                        fileStreamOut.Close();
+                    }
+
+                    entry = zipInStream.GetNextEntry();
+                }
+                zipInStream.Close();
+                fileStreamIn.Close();
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
