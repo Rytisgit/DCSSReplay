e01261cf9d6ed8561a0e8a77772bd0f83e690e01
Rytis Petronis <petronisrytis@gmail.com>
2020-05-16 12:36:14 +0300

    remove many unneded things, remove console

--- Putty/WINDOWS/MSVC/PUTTYDLL/export.c
+++ Putty/WINDOWS/MSVC/PUTTYDLL/export.c
@@ -20,27 +20,27 @@ static Conf* get_default_config() {
 	return conf;*/
 	return NULL;
 }
-void CreateConsole()
-{
-	if (!AllocConsole()) {
-		// Add some error handling here.
-		// You can call GetLastError() to get more info about the error.
-		return;
-	}
-
-	// std::cout, std::clog, std::cerr, std::cin
-	FILE* fDummy;
-	freopen_s(&fDummy, "CONOUT$", "w", stdout);
-	freopen_s(&fDummy, "CONOUT$", "w", stderr);
-	freopen_s(&fDummy, "CONIN$", "r", stdin);
-
-}
+//void CreateConsole()
+//{
+//	if (!AllocConsole()) {
+//		// Add some error handling here.
+//		// You can call GetLastError() to get more info about the error.
+//		return;
+//	}
+//
+//	// std::cout, std::clog, std::cerr, std::cin
+//	FILE* fDummy;
+//	freopen_s(&fDummy, "CONOUT$", "w", stdout);
+//	freopen_s(&fDummy, "CONOUT$", "w", stderr);
+//	freopen_s(&fDummy, "CONIN$", "r", stdin);
+//
+//}
 EXPORT(Terminal*) CreatePuttyTerminal( int w, int h ) {
 	int i;
 	struct unicode_data* unicode;
 	Terminal* terminal;
-	CreateConsole();
-	printf("TESTSSETSETSETSET");
+	//CreateConsole();
+	//printf("TESTSSETSETSETSET");
 	unicode = snew(struct unicode_data);
 	memset( unicode, 0, sizeof(struct unicode_data) );
 
