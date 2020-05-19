1cb9ed0a91e43ddccb77a904c59a0e3d12687d2d
Rytis Petronis <petronisrytis@gmail.com>
2020-05-19 20:50:21 +0300

    dll example

---
+++ DCSSTV2/Program.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace DCSSTV2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var accessor = new ResourceAccessor(Assembly.GetExecutingAssembly());
+            var libManager = new LibraryManager(
+                Assembly.GetExecutingAssembly(),
+                //new LibraryItem(Platform.MacOs, Bitness.x32,
+                //    new LibraryFile("libPuttyDLL.dylib", accessor.Binary("libPuttyDLL.dylib"))),
+                new LibraryItem(Platform.Windows, Bitness.x64,
+                    new LibraryFile("libPuttyDLL.dll", accessor.Binary("libPuttyDLL.dll")))
+                //,
+                //new LibraryItem(Platform.Linux, Bitness.x32,
+                //    new LibraryFile("libPuttyDLL.so", accessor.Binary("libPuttyDLL.so")))
+                );
+
+            libManager.LoadNativeLibrary();
+
+            //frameGenerator = new MainGenerator(System.IO.Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), @"Extra"));
+
+            var term = new Putty.Terminal(80, 24);
+            Console.WriteLine("hah");
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
--- DCSSTVBroadcaster/Program.cs
+++ DCSSTVBroadcaster/Program.cs
@@ -36,184 +36,7 @@ namespace DCSSTVBroadcaster
         private bool run = true;
 
 
-        void DoOpenFiles()
-        {
-            var delay = TimeSpan.Zero;
-
-            //if (files.Length > 1)
-            //{
-            //    // multiselect!
-            //    var fof = new FileOrderingForm(files);
-            //    if (fof.ShowDialog(this) != DialogResult.OK) return;
-            //    files = fof.FileOrder.ToArray();
-            //    delay = TimeSpan.FromSeconds(fof.SecondsBetweenFiles);
-            //}
-
-            fileStream.CopyTo(destination);
-            var streams = new List<Stream> { destination };
-            ttyrecDecoder = new TtyRecKeyframeDecoder(80, 24, streams, delay, MaxDelayBetweenPackets);
-            PlaybackSpeed = +1;
-            Seek = TimeSpan.Zero;
-        }
-
-        //private IEnumerable<Stream> TtyrecToStream()
-        //{
-        //    return files.Select(f =>
-        //    {
-
-        //        if (Path.GetExtension(f) == ".ttyrec") return File.OpenRead(f);
-        //        Stream streamCompressed = File.OpenRead(f);
-        //        Stream streamUncompressed = new MemoryStream();
-        //        if (Path.GetExtension(f) == ".bz2")
-        //        {
-        //            try
-        //            {
-        //                BZip2.Decompress(streamCompressed, streamUncompressed, false);
-        //            }
-        //            catch
-        //            {
-        //               // MessageBox.Show("The file is corrupted or not supported");
-        //            }
-        //            return streamUncompressed;
-        //        }
-        //        if (Path.GetExtension(f) == ".gz")
-        //        {
-        //            try
-        //            {
-        //                GZip.Decompress(streamCompressed, streamUncompressed, false);
-        //            }
-        //            catch
-        //            {
-        //                //MessageBox.Show("The file is corrupted or not supported");
-        //            }
-        //            return streamUncompressed;
-        //        }
-        //        return null;
-        //    });
-        //}
-        //private IEnumerable<Stream> TtyrecToStream(Dictionary<string, Stream> s)
-        //{
-        //    return s.Select(f =>
-        //    {
-
-        //        if (f.Key == "ttyrec") return f.Value;
-        //        Stream streamCompressed = f.Value;
-        //        Stream streamUncompressed = new MemoryStream();
-        //        if (f.Key == "bz2")
-        //        {
-        //            try
-        //            {
-        //                BZip2.Decompress(streamCompressed, streamUncompressed, false);
-        //            }
-        //            catch
-        //            {
-        //                MessageBox.Show("The file is corrupted or not supported");
-        //            }
-        //            return streamUncompressed;
-        //        }
-        //        if (f.Key == "gz")
-        //        {
-        //            try
-        //            {
-        //                GZip.Decompress(streamCompressed, streamUncompressed, false);
-        //            }
-        //            catch
-        //            {
-        //                MessageBox.Show("The file is corrupted or not supported");
-        //            }
-        //            return streamUncompressed;
-        //        }
-        //        return null;
-        //    });
-        //}
-
-        void MainLoop()
-        {
-            var now = DateTime.Now;
-
-            PreviousFrames.Add(now);
-            PreviousFrames.RemoveAll(f => f.AddSeconds(1) < now);
-
-            var dt = Math.Max(0, Math.Min(0.1, (now - PreviousFrame).TotalSeconds));
-            PreviousFrame = now;
-
-            if (ttyrecDecoder != null)
-            {
-
-                Seek += TimeSpan.FromSeconds(dt * PlaybackSpeed);
-
-                if (Seek > ttyrecDecoder.Length)
-                {
-                    Seek = ttyrecDecoder.Length;
-                }
-                if (Seek < TimeSpan.Zero)
-                {
-                    Seek = TimeSpan.Zero;
-                }
-
-                if (FrameStepCount != 0)
-                {
-                    ttyrecDecoder.FrameStep(FrameStepCount); //step frame index by count
-                    Seek = ttyrecDecoder.CurrentFrame.SinceStart;
-                    FrameStepCount = 0;
-                }
-                else
-                {
-
-                    ttyrecDecoder.Seek(Seek);
-
-                }
-
-                var frame = ttyrecDecoder.CurrentFrame.Data;
-
-                if (frame != null)
-                {
-
-                    if (!frameGenerator.isGeneratingFrame)
-                    {
-                        frameGenerator.isGeneratingFrame = true;
-#if false
-                        ThreadPool.UnsafeQueueUserWorkItem(o =>
-                        {
-                            try
-                            {
-                                bmp = frameGenerator.GenerateImage(frame, ConsoleSwitchLevel);
-                                canvasView.InvalidateSurface();
-                                frameGenerator.isGeneratingFrame = false;
-                                frame = null;
-                            }
-                            catch (Exception ex)
-                            {
-                                Console.WriteLine(ex.Message);
-                                //generator.GenerateImage(savedFrame);
-                                frameGenerator.isGeneratingFrame = false;
-                            }
-                        }, null);
-#else //non threaded image generation (slow)
-                        //bmp = frameGenerator.GenerateImage(frame, ConsoleSwitchLevel);
-                        //canvasView.InvalidateSurface();
-
-                        frameGenerator.isGeneratingFrame = false;
-#endif
-                    }
-                }
-
-            }
-
-        }
-        static void Loop()
-        {
-            while (run)
-            {
-                MainLoop();
-            }
-        }
-        static void Runner()
-        {
-            DoOpenFiles();
-            Thread m_Thread = new Thread(() => Loop());
-            m_Thread.Start();
-        }
+       
 
         public static void Main(string[] args)
         {
@@ -222,7 +45,7 @@ namespace DCSSTVBroadcaster
                 Assembly.GetExecutingAssembly(),
                 //new LibraryItem(Platform.MacOs, Bitness.x32,
                 //    new LibraryFile("libPuttyDLL.dylib", accessor.Binary("libPuttyDLL.dylib"))),
-                new LibraryItem(Platform.Windows, Bitness.x32,
+                new LibraryItem(Platform.Windows, Bitness.x64,
                     new LibraryFile("libPuttyDLL.dll", accessor.Binary("libPuttyDLL.dll")))
                 //,
                 //new LibraryItem(Platform.Linux, Bitness.x32,
@@ -231,10 +54,9 @@ namespace DCSSTVBroadcaster
 
             libManager.LoadNativeLibrary();
 
-            frameGenerator = new MainGenerator(System.IO.Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), @"Extra"));
+            //frameGenerator = new MainGenerator(System.IO.Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), @"Extra"));
 
-            fileStream = file;
-            Runner();
+            var term = new Putty.Terminal(80, 24);
             Console.WriteLine("hah");
         }
 
