664f8d556428aac22efd134a580746bd9beed6b8
Aspectrus <eextreem0@gmail.com>
2020-05-19 21:38:50 +0300

    wow

--- DCSSTV2/Program.cs
+++ DCSSTV2/Program.cs
@@ -2,12 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NativeLibraryManager;
 
 namespace DCSSTV2
 {
---
+++ DCSSTV2/Streams.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DCSSTV2.Streams
+{
+    public static class Streams
+    {
+        public static IEnumerable<Stream> TtyrecToStream(this List<string> files)
+        {
+            return files.Select(f =>
+            {
+                return File.OpenRead(f);
+            });
+        }
+    }
+}
