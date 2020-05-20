b89678ca16ff26724e5c9bb674e1e3c92f0ac374
Aspectrus <eextreem0@gmail.com>
2020-05-20 16:40:03 +0300

    Can upload file now

--- DCSSTV2/Streams.cs
+++ DCSSTV2/Streams.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BlazorInputFile;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -15,5 +16,7 @@ namespace DCSSTV2.Streams
                 return File.OpenRead(f);
             });
         }
+
+  
     }
 }
