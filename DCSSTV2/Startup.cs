ed6853d0d9ef21ce4c06cbaff8dba6b9f0f59015
Rytis Petronis <petronisrytis@gmail.com>
2020-06-04 08:24:49 +0300

    update base path for hosting

--- DCSSTV2/Startup.cs
+++ DCSSTV2/Startup.cs
@@ -47,7 +47,7 @@ namespace DCSSTV2
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.UsePathBase("/dcss");
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
